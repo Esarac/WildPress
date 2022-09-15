105c8f50a888f1c57d45460015c04f55c5d3174c
Esteban Ariza Acosta <48232743+Esarac@users.noreply.github.com>
2022-09-14 19:44:37 -0500

    First Commit :muscle:

---
+++ app/src/main/java/com/example/test170822/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.test170822
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.Button
+import android.widget.EditText
+import com.example.test170822.models.Exercise
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var layoutManager: RecyclerView.LayoutManager
+    private lateinit var adapter: ExerciseAdapter
+    private lateinit var recyclerView: RecyclerView
+
+    private lateinit var createButton: Button
+    private lateinit var nameEditText: EditText
+    private lateinit var descriptionEditText: EditText
+
+    private var exercises = ArrayList<Exercise>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        this.exercises.add(Exercise("Push up", "Exercise n1"))
+        this.exercises.add(Exercise("Incline up", "Exercise n2"))
+        this.exercises.add(Exercise("Decline up", "Exercise n3"))
+
+        this.layoutManager = LinearLayoutManager(this)
+        this.adapter = ExerciseAdapter(this.exercises)
+        this.recyclerView = findViewById(R.id.exerciseRecyclerView)
+        this.recyclerView.layoutManager = this.layoutManager
+        this.recyclerView.adapter = this.adapter
+
+        this.createButton = findViewById(R.id.createButton)
+        this.nameEditText = findViewById(R.id.nameEditText)
+        this.descriptionEditText = findViewById(R.id.descriptionEditText)
+
+        createButton.setOnClickListener{
+            val exercise = Exercise(nameEditText.text.toString(), descriptionEditText.text.toString())
+            adapter.addExercise(exercise)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/test170822/models/Exercise.kt
@@ -0,0 +1,3 @@
+package com.example.test170822.models
+
+class Exercise(val name: String = "", val description: String = "")
