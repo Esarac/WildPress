37409a0e1d1e4b8d39b0602c3c65e76d195b56fe
Johan Sebastián Giraldo Rubio <j.sebasgr036@gmail.com>
2022-10-04 18:25:22 -0500

    Add sticky buttons in workout

--- app/src/main/java/com/wildpress/activities/CreateDietActivity.kt
+++ app/src/main/java/com/wildpress/activities/CreateDietActivity.kt
@@ -2,15 +2,30 @@ package com.wildpress.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.wildpress.R
 import com.wildpress.components.Toolbar
+import com.wildpress.databinding.ActivityCreateDietBinding
 
 class CreateDietActivity : AppCompatActivity() {
+
+    //Binding
+    private lateinit var binding : ActivityCreateDietBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_diet)
+        binding = ActivityCreateDietBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        //Initialize toolbar
         Toolbar().showToolbar(this, "New Diet", true)
+
+        //Listeners
+        binding.dietCreCancelBtn.setOnClickListener {
+            onSupportNavigateUp()
+        }
+
+        binding.dietCreSubmitBtn.setOnClickListener {
+            onSupportNavigateUp()
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
