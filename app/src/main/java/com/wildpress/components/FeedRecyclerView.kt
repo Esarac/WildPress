ec7d82de1422b88ebec81ba095ba2ab70a945d77
Samuel Satizabal <samuelsatizabal@hotmail.com>
2022-10-10 18:39:57 -0500

    Workin' on feed page 🥵

---
+++ app/src/main/java/com/wildpress/components/FeedRecyclerView.kt
@@ -0,0 +1,9 @@
+package com.wildpress.components
+
+import androidx.recyclerview.widget.RecyclerView
+import com.wildpress.model.Post
+
+class FeedRecyclerView (posts: ArrayList<Post>) {
+//   : RecyclerView.Adapter<PostRecyclerView.PostViewHolder>()
+    private val posts: ArrayList<Post> = posts;
+}
