4591f6290fc8dc9cffa0f11a6ef305da0c092caf
Johan Sebastián Giraldo Rubio <j.sebasgr036@gmail.com>
2022-10-12 19:38:12 -0500

    Add Logout button in Profile screen

--- app/src/main/java/com/wildpress/fragments/Profile.kt
+++ app/src/main/java/com/wildpress/fragments/Profile.kt
@@ -1,13 +1,21 @@
 package com.wildpress.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.wildpress.R
+import com.wildpress.activities.CreateDietActivity
+import com.wildpress.activities.LoginActivity
+import com.wildpress.databinding.FragmentDietBinding
+import com.wildpress.databinding.FragmentProfileBinding
 
 class Profile : Fragment() {
+
+    private var _binding : FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +25,16 @@ class Profile : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.profileLogoutBtn.setOnClickListener {
+            startActivity(Intent(activity, LoginActivity::class.java))
+        }
+    }
+
 }
