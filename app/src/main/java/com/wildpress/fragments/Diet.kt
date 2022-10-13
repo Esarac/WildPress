0f05ca2080fd119770d760200500c85f6ea47a54
Esteban Ariza Acosta <48232743+Esarac@users.noreply.github.com>
2022-10-12 19:40:05 -0500

    List diet

--- app/src/main/java/com/wildpress/fragments/Diet.kt
+++ app/src/main/java/com/wildpress/fragments/Diet.kt
@@ -6,17 +6,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.wildpress.R
 import com.wildpress.activities.CreateDietActivity
+import com.wildpress.components.CardRecyclerView
 import com.wildpress.databinding.FragmentDietBinding
+import com.wildpress.model.Diet
+import com.wildpress.model.Workout
 
 class Diet : Fragment(R.layout.fragment_diet) {
 
     private var _binding : FragmentDietBinding? = null
     private val binding get() = _binding!!
 
+    //Properties
+    private lateinit var layoutManager: RecyclerView.LayoutManager
+    private lateinit var adapter: CardRecyclerView<Diet>
+    private var diets = ArrayList<Diet>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        this.diets.add(Diet("Trucha a la plancha", "Diet n1", "Cocinarla"))
+        this.diets.add(Diet("Batido de proteina", "Diet n2", "Batirlo"))
     }
 
     override fun onCreateView(
@@ -29,6 +42,11 @@ class Diet : Fragment(R.layout.fragment_diet) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        this.layoutManager = LinearLayoutManager(context)
+        this.adapter = CardRecyclerView(this.diets)
+        binding.dietRecyclerView.layoutManager = this.layoutManager
+        binding.dietRecyclerView.adapter = this.adapter
 
         binding.createDietBtn.setOnClickListener {
             startActivity(Intent(activity, CreateDietActivity::class.java))
---
+++ app/src/main/java/com/wildpress/model/Diet.kt
@@ -0,0 +1,13 @@
+package com.wildpress.model
+
+import com.wildpress.R
+
+class Diet(val name: String = "", val description: String = "", val ingredients: String = ""): Cardable {
+    override fun getImage(): Int {
+        return return R.drawable.ic_google
+    }
+
+    override fun getTitle(): String {
+        return name
+    }
+}
