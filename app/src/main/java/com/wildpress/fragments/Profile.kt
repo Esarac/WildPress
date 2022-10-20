025ff539b40a997909041d043e79dd91ac016320
Samuel <samuelsatizabal@hotmail.com>
2022-10-19 21:00:01 -0500

    added the profile image tool for uploading a photo 😎

--- app/src/main/java/com/wildpress/fragments/Profile.kt
+++ app/src/main/java/com/wildpress/fragments/Profile.kt
@@ -1,11 +1,15 @@
 package com.wildpress.fragments
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import com.wildpress.activities.LoginActivity
 import com.wildpress.databinding.FragmentProfileBinding
 
@@ -13,6 +17,7 @@ class Profile : Fragment() {
 
     private var _binding : FragmentProfileBinding? = null
     private val binding get() = _binding!!
+    private var imageUri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +33,22 @@ class Profile : Fragment() {
         binding.profileLogoutBtn.setOnClickListener {
             startActivity(Intent(activity, LoginActivity::class.java))
         }
+
+        binding.editProfilePictureBtn.setOnClickListener {
+            openGallery()
+        }
+    }
+
+    private val startGalleryForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            imageUri = it.data?.data
+            binding.profProfilePicture.setImageURI(imageUri)
+        }
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startGalleryForResult.launch(intent)
     }
 
 }
