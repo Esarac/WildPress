2f97bcdfa167baca49b6a610e25eaea20f4877ae
JuanOssa1 <juan.ossa2@u.icesi.edu.co>
2022-10-26 12:23:32 -0500

    Firebase connection added

--- app/src/main/java/com/wildpress/activities/SignInActivity.kt
+++ app/src/main/java/com/wildpress/activities/SignInActivity.kt
@@ -2,6 +2,10 @@ package com.wildpress.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.wildpress.components.Toolbar
 import com.wildpress.databinding.ActivitySignInBinding
 
@@ -19,13 +23,20 @@ class SignInActivity : AppCompatActivity() {
         Toolbar().showToolbar(this, "Sign In", true)
 
         //Listeners
-        binding.sigInSignInBtn.setOnClickListener {
-            onSupportNavigateUp()
-        }
+        binding.sigInSignInBtn.setOnClickListener(::register)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressedDispatcher.onBackPressed()
         return true
+    }
+    private fun register(view: View){
+        Log.e("prueba", binding.signInEmailTextEdit.text.toString())
+        Firebase.auth.createUserWithEmailAndPassword(
+            binding.signInEmailTextEdit.text.toString(),
+            binding.signInPasswordTextEdit.text.toString()
+        ).addOnSuccessListener {
+            finish();
+        }
     }
 }
